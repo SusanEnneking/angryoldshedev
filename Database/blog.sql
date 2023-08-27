ae86c825b8a93100ea5f69ff9e2a8955b33415c5
Susan Enneking <susan.enneking@gmail.com>
2023-08-27 16:45:47 -0500

    add a few columns for history

--- Database/blog.sql
+++ Database/blog.sql
@@ -5,3 +5,8 @@ CREATE TABLE `blogs`.`blog` (
   `generated` LONGTEXT NULL,
   `written` LONGTEXT NULL COMMENT '	',
   PRIMARY KEY (`id`));
+
+ALTER TABLE `blogs`.`blog` 
+ADD COLUMN `created_date` DATETIME NULL AFTER `written`,
+ADD COLUMN `created_by` VARCHAR(45) NULL AFTER `created_date`,
+ADD COLUMN `is_active` VARCHAR(45) NULL AFTER `created_by`;
