565af43072f29a1ee7d6180495fab6a3f3797716
Susan Enneking <susan.enneking@gmail.com>
2023-08-28 03:53:29 -0500

    category fk

--- Database/blogs.sql
+++ Database/blogs.sql
@@ -11,10 +11,5 @@ ADD COLUMN `created_date` DATETIME NULL AFTER `written`,
 ADD COLUMN `created_by` VARCHAR(45) NULL AFTER `created_date`,
 ADD COLUMN `is_active` VARCHAR(45) NULL AFTER `created_by`;
 
-CREATE TABLE `blogs`.`categories` (
-  `id` INT GENERATED ALWAYS AS (),
-  `category` VARCHAR(45) NOT NULL,
-  `created_by` VARCHAR(45) NULL,
-  `created_date` DATETIME NOT NULL,
-  `is_active` TINYINT NULL,
-  PRIMARY KEY (`id`));
+ALTER TABLE `blogs`.`blog` 
+ADD COLUMN `category` INT NULL AFTER `is_active`;
