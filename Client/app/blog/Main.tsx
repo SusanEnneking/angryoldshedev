4e28e963e48f712b14af43aa72eaa4bece6aaba7
Susan Enneking <susan.enneking@gmail.com>
2023-12-21 09:36:45 -0600

    #3 basic blog layout looks good

---
+++ Client/app/blog/Main.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import { MDXProps } from 'mdx/types';
+import Markdown from './Markdown';
+
+interface MainProps {
+  posts: MDXProps[];
+  title: string;
+}
+
+export default function Main(props: MainProps) {
+  const { posts, title } = props;
+
+  return (
+    <Grid
+      item
+      xs={12}
+      md={8}
+      sx={{
+        '& .markdown': {
+          py: 3,
+        },
+      }}
+    >
+      <Typography variant="h6" gutterBottom>
+        {title}
+      </Typography>
+      <Divider />
+      {posts.map((post) => (
+        <Markdown className="markdown">
+          {post}
+        </Markdown>
+      ))}
+    </Grid>
+  );
+}
